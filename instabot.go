1ac051c204ddf859e3ea6056f526a314c4648541
Shahzil Abid <shahzilabid@Shahzils-MacBook-Pro.local>
2018-07-18 15:50:49 -0700

    Initial

--- instabot.go
+++ instabot.go
@@ -1,16 +1,210 @@
 package main
 
+// TODO:
+// // FIGRE OUT PAGINATION (MAX ID)
+// WE KEEP ITERATING TILL WE HIT CAPS
+
+// FOLLOWERS FEED
+// INCORPORATE PROXY (Have a pool which gets assigned on each CreateUser call)
+// STATS
+// ONE TYPE PER ACCOUNT?
+// FOLLOWERS LOGIC
+// BILLING
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/boltdb/bolt"
+	"github.com/missionMeteora/instabot/misc"
+	"github.com/tducasse/goinsta"
+	"github.com/tducasse/goinsta/store"
+
+	"github.com/missionMeteora/apiserv"
+)
+
+type Server struct {
+	r  *apiserv.Server
+	db *bolt.DB
+}
+
+const (
+	BUCKET = "users"
+	PORT   = "8080"
+)
+
+// Run starts the server
+func (srv *Server) Run() error {
+	var (
+		errCh = make(chan error, 1)
+	)
+
+	go func() {
+		log.Printf("listening on http://:" + PORT)
+		errCh <- srv.r.Run(":" + PORT)
+	}()
+	return <-errCh
+}
+
 func main() {
-	// Gets the command line options
-	parseOptions()
-	// Gets the config
-	getConfig()
-	// Tries to login
-	login()
-	if *unfollow {
-		syncFollowers()
-	} else if *run {
-		// Loop through tags ; follows, likes, and comments, according to the config file
-		loopTags()
+	// Create path for DB
+	os.MkdirAll("./data", 0700)
+
+	rand.Seed(time.Now().UnixNano())
+	r := apiserv.New()
+
+	// Open Bolt DB
+	db := misc.OpenDB("./data/", "instabot")
+	log.Println("Initializing bucket", BUCKET)
+	if err := db.Update(func(tx *bolt.Tx) error {
+		log.Println("Initializing bucket", BUCKET)
+		if _, err := tx.CreateBucketIfNotExists([]byte(BUCKET)); err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+
+		idxStart := uint64(1)
+		if err := misc.InitIndex(tx, BUCKET, idxStart); err != nil {
+			return err
+		}
+
+		return nil
+	}); err != nil {
+		log.Fatalln(err)
 	}
+
+	srv := &Server{
+		r:  r,
+		db: db,
+	}
+
+	blah := Range{Min: 0, Max: 4000}
+	lm := Limit{
+		MaxRetry: 2,
+		Like:     blah,
+		Comment:  blah,
+		Follow:   blah,
+	}
+
+	locations := make(map[string]Cap)
+	locations["zoo"] = Cap{
+		Like:    5,
+		Comment: 5,
+		Follow:  5,
+	}
+
+	usr := &User{
+		Email: "shahzilabid@gmail.com",
+		Targets: []*Target{&Target{
+			Tags:     locations,
+			Limit:    lm,
+			Name:     "SHIT",
+			Comments: []string{"Where is this?", "I love the zoo"}}},
+		Username: "swayshah",
+		Password: "muchodinero",
+	}
+	log.Println("Creating user", SaveUser(usr, db))
+
+	go srv.Act()
+
+	// r.GET("/api", byRange(srv))
+	r.GET("/ping", ping(srv))
+	// r.GET("/hack", hack(srv))
+	// r.GET("/byDay", byDay(srv))
+
+	// Listen and Serve
+	if err := srv.Run(); err != nil {
+		log.Panicf("Failed to listen: %v", err)
+	}
+
+	// // Gets the command line options
+	// parseOptions()
+	// // Gets the config
+	// getConfig()
+	// // Tries to login
+	// login()
+	// if *unfollow {
+	// 	syncFollowers()
+	// } else if *run {
+	// 	// Loop through tags ; follows, likes, and comments, according to the config file
+	// 	loopTags()
+	// }
+
+}
+
+func ping(s *Server) apiserv.Handler {
+	return func(c *apiserv.Context) apiserv.Response {
+		return apiserv.PlainResponse("", "pong")
+	}
+}
+
+func (s *Server) Act() {
+	for {
+		for _, usr := range GetAllUsers(s.db) {
+			// if usr.LastRun > 0 && withinLast(usr.LastRun, 60*60*24) {
+			// 	continue
+			// }
+
+			// Get config
+			var (
+				insta *goinsta.Instagram
+				err   error
+			)
+			if len(usr.Session) == 0 || len(usr.Key) == 0 {
+				log.Println("NEW!")
+				insta = goinsta.NewViaProxy(usr.Username, usr.Password, usr.Proxy)
+				if err := insta.Login(); err != nil {
+					log.Println("Error logging in", usr.Email, err)
+					continue
+				}
+
+				key := createKey()
+				bytes, err := store.Export(insta, key)
+				if err != nil {
+					log.Println("Error exporting login", usr.Email, err)
+					continue
+				}
+
+				usr.Key = key
+				usr.Session = bytes
+			} else {
+				log.Println("OLD!")
+				insta, err = usr.GetLogin()
+				if err != nil {
+					log.Println("Error logging in from import", usr.Email, err)
+					continue
+				}
+			}
+
+			// log.Println("DONE!", len(usr.Session), len(usr.Key))
+			for _, tg := range usr.Targets {
+				if len(tg.Tags) > 0 {
+					loopTags(usr.Username, insta, tg)
+				}
+
+				if len(tg.Locations) > 0 {
+					loopLocations(usr.Username, insta, tg)
+				}
+
+				if len(tg.Followers) > 0 {
+				}
+			}
+
+			usr.LastRun = time.Now().Unix()
+
+			if err := SaveUser(usr, s.db); err != nil {
+				log.Println("Error saving user", usr.Email, err)
+				continue
+			}
+
+		}
+
+		time.Sleep(20 * time.Minute)
+	}
+}
+
+func withinLast(timestamp, seconds int64) bool {
+	return timestamp >= (time.Now().Unix() - seconds)
 }
--- util.go
+++ util.go
@@ -1,166 +1,163 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io"
 	"log"
-	"net/smtp"
-	"os"
-	"strings"
+	"math/rand"
 	"time"
 
-	"github.com/spf13/viper"
+	"github.com/tducasse/goinsta"
+	"github.com/tducasse/goinsta/response"
 )
 
 // Whether we are in development mode or not
-var dev *bool
+// var dev *bool
 
-// Whether we want an email to be sent when the script ends / crashes
-var nomail *bool
+// // Whether we want an email to be sent when the script ends / crashes
+// var nomail *bool
 
-// Whether we want to launch the unfollow mode
-var unfollow *bool
+// // Whether we want to launch the unfollow mode
+// var unfollow *bool
 
-// Acut
-var run *bool
+// // Acut
+// var run *bool
 
-// An image will be liked if the poster has more followers than likeLowerLimit, and less than likeUpperLimit
-var likeLowerLimit int
-var likeUpperLimit int
+// // An image will be liked if the poster has more followers than likeLowerLimit, and less than likeUpperLimit
+// var likeLowerLimit int
+// var likeUpperLimit int
 
-// A user will be followed if he has more followers than followLowerLimit, and less than followUpperLimit
-// Needs to be a subset of the like interval
-var followLowerLimit int
-var followUpperLimit int
+// // A user will be followed if he has more followers than followLowerLimit, and less than followUpperLimit
+// // Needs to be a subset of the like interval
+// var followLowerLimit int
+// var followUpperLimit int
 
-// An image will be commented if the poster has more followers than commentLowerLimit, and less than commentUpperLimit
-// Needs to be a subset of the like interval
-var commentLowerLimit int
-var commentUpperLimit int
+// // An image will be commented if the poster has more followers than commentLowerLimit, and less than commentUpperLimit
+// // Needs to be a subset of the like interval
+// var commentLowerLimit int
+// var commentUpperLimit int
 
-// Hashtags list. Do not put the '#' in the config file
-var tagsList map[string]interface{}
+// // Hashtags list. Do not put the '#' in the config file
+// var tagsList map[string]interface{}
 
-// Limits for the current hashtag
-var limits map[string]int
+// // Limits for the current hashtag
+// var limits map[string]int
 
-// Comments list
-var commentsList []string
+// // Comments list
+// var commentsList []string
 
 // Line is a struct to store one line of the report
-type line struct {
-	Tag, Action string
-}
+// type line struct {
+// 	Tag, Action string
+// }
 
 // Report that will be sent at the end of the script
-var report map[line]int
+// var report map[line]int
 
-// Counters that will be incremented while we like, comment, and follow
-var numFollowed int
-var numLiked int
-var numCommented int
+// // Counters that will be incremented while we like, comment, and follow
+// var numFollowed int
+// var numLiked int
+// var numCommented int
 
 // Will hold the tag value
-var tag string
+// var tag string
 
 // check will log.Fatal if err is an error
-func check(err error) {
-	if err != nil {
-		log.Fatal("ERROR:", err)
-	}
-}
+// func check(err error) {
+// 	if err != nil {
+// 		log.Fatal("ERROR:", err)
+// 	}
+// }
 
-// Parses the options given to the script
-func parseOptions() {
-	run = flag.Bool("run", false, "Use this option to follow, like and comment")
-	unfollow = flag.Bool("sync", false, "Use this option to unfollow those who are not following back")
-	nomail = flag.Bool("nomail", false, "Use this option to disable the email notifications")
-	dev = flag.Bool("dev", false, "Use this option to use the script in development mode : nothing will be done for real")
-	logs := flag.Bool("logs", false, "Use this option to enable the logfile")
+// // Parses the options given to the script
+// func parseOptions() {
+// 	run = flag.Bool("run", false, "Use this option to follow, like and comment")
+// 	unfollow = flag.Bool("sync", false, "Use this option to unfollow those who are not following back")
+// 	nomail = flag.Bool("nomail", false, "Use this option to disable the email notifications")
+// 	dev = flag.Bool("dev", false, "Use this option to use the script in development mode : nothing will be done for real")
+// 	logs := flag.Bool("logs", false, "Use this option to enable the logfile")
 
-	flag.Parse()
+// 	flag.Parse()
 
-	// -logs enables the log file
-	if *logs {
-		// Opens a log file
-		t := time.Now()
-		logFile, err := os.OpenFile("instabot-"+t.Format("2006-01-02-15-04-05")+".log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
-		check(err)
-		defer logFile.Close()
+// 	// -logs enables the log file
+// 	if *logs {
+// 		// Opens a log file
+// 		t := time.Now()
+// 		logFile, err := os.OpenFile("instabot-"+t.Format("2006-01-02-15-04-05")+".log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+// 		check(err)
+// 		defer logFile.Close()
 
-		// Duplicates the writer to stdout and logFile
-		mw := io.MultiWriter(os.Stdout, logFile)
-		log.SetOutput(mw)
-	}
-}
+// 		// Duplicates the writer to stdout and logFile
+// 		mw := io.MultiWriter(os.Stdout, logFile)
+// 		log.SetOutput(mw)
+// 	}
+// }
 
 // Gets the conf in the config file
-func getConfig() {
-	folder := "config"
-	if *dev {
-		folder = "local"
-	}
-	viper.SetConfigFile(folder + "/config.json")
+// func getConfig() {
+// 	folder := "config"
+// 	if *dev {
+// 		folder = "local"
+// 	}
+// 	viper.SetConfigFile(folder + "/config.json")
 
-	// Reads the config file
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("Error reading config file, %s", err)
-	}
+// 	// Reads the config file
+// 	if err := viper.ReadInConfig(); err != nil {
+// 		log.Fatalf("Error reading config file, %s", err)
+// 	}
 
-	// Confirms which config file is used
-	log.Printf("Using config: %s\n\n", viper.ConfigFileUsed())
+// 	// Confirms which config file is used
+// 	log.Printf("Using config: %s\n\n", viper.ConfigFileUsed())
 
-	likeLowerLimit = viper.GetInt("limits.like.min")
-	likeUpperLimit = viper.GetInt("limits.like.max")
+// 	likeLowerLimit = viper.GetInt("limits.like.min")
+// 	likeUpperLimit = viper.GetInt("limits.like.max")
 
-	followLowerLimit = viper.GetInt("limits.follow.min")
-	followUpperLimit = viper.GetInt("limits.follow.max")
+// 	followLowerLimit = viper.GetInt("limits.follow.min")
+// 	followUpperLimit = viper.GetInt("limits.follow.max")
 
-	commentLowerLimit = viper.GetInt("limits.comment.min")
-	commentUpperLimit = viper.GetInt("limits.comment.max")
+// 	commentLowerLimit = viper.GetInt("limits.comment.min")
+// 	commentUpperLimit = viper.GetInt("limits.comment.max")
 
-	tagsList = viper.GetStringMap("tags")
+// 	tagsList = viper.GetStringMap("tags")
 
-	commentsList = viper.GetStringSlice("comments")
+// 	commentsList = viper.GetStringSlice("comments")
 
-	type Report struct {
-		Tag, Action string
-	}
+// 	type Report struct {
+// 		Tag, Action string
+// 	}
 
-	report = make(map[line]int)
-}
+// 	report = make(map[line]int)
+// }
 
-// Sends an email. Check out the "mail" section of the "config.json" file.
-func send(body string, success bool) {
-	if !*nomail {
-		from := viper.GetString("user.mail.from")
-		pass := viper.GetString("user.mail.password")
-		to := viper.GetString("user.mail.to")
+// // Sends an email. Check out the "mail" section of the "config.json" file.
+// func send(body string, success bool) {
+// 	if !*nomail {
+// 		from := viper.GetString("user.mail.from")
+// 		pass := viper.GetString("user.mail.password")
+// 		to := viper.GetString("user.mail.to")
 
-		status := func() string {
-			if success {
-				return "Success!"
-			}
-			return "Failure!"
-		}()
-		msg := "From: " + from + "\n" +
-			"To: " + to + "\n" +
-			"Subject:" + status + "  go-instabot\n\n" +
-			body
+// 		status := func() string {
+// 			if success {
+// 				return "Success!"
+// 			}
+// 			return "Failure!"
+// 		}()
+// 		msg := "From: " + from + "\n" +
+// 			"To: " + to + "\n" +
+// 			"Subject:" + status + "  go-instabot\n\n" +
+// 			body
 
-		err := smtp.SendMail(viper.GetString("user.mail.smtp"),
-			smtp.PlainAuth("", from, pass, viper.GetString("user.mail.server")),
-			from, []string{to}, []byte(msg))
+// 		err := smtp.SendMail(viper.GetString("user.mail.smtp"),
+// 			smtp.PlainAuth("", from, pass, viper.GetString("user.mail.server")),
+// 			from, []string{to}, []byte(msg))
 
-		if err != nil {
-			log.Printf("smtp error: %s", err)
-			return
-		}
+// 		if err != nil {
+// 			log.Printf("smtp error: %s", err)
+// 			return
+// 		}
 
-		log.Print("sent")
-	}
-}
+// 		log.Print("sent")
+// 	}
+// }
 
 // Retries the same function [function], a certain number of times (maxAttempts).
 // It is exponential : the 1st time it will be (sleep), the 2nd time, (sleep) x 2, the 3rd time, (sleep) x 3, etc.
@@ -177,44 +174,133 @@ func retry(maxAttempts int, sleep time.Duration, function func() error) (err err
 		log.Println("Retrying after error:", err)
 	}
 
-	send(fmt.Sprintf("The script has stopped due to an unrecoverable error :\n%s", err), false)
+	// Add email?
+	log.Println("The script has stopped due to an unrecoverable error :\n%s", err)
 	return fmt.Errorf("After %d attempts, last error: %s", maxAttempts, err)
 }
 
-// Builds the line for the report and prints it
-func buildLine() {
-	reportTag := ""
-	for index, element := range report {
-		if index.Tag == tag {
-			reportTag += fmt.Sprintf("%s %d/%d - ", index.Action, element, limits[index.Action])
-		}
-	}
-	// Prints the report line on the screen / in the log file
-	if reportTag != "" {
-		log.Println(strings.TrimSuffix(reportTag, " - "))
+// createKey creates a key and saves it to file
+func createKey() []byte {
+	key := make([]byte, 32)
+	rand.Read(key)
+	return key
+}
+
+// // Builds the line for the report and prints it
+// func buildLine() {
+// 	reportTag := ""
+// 	for index, element := range report {
+// 		if index.Tag == tag {
+// 			reportTag += fmt.Sprintf("%s %d/%d - ", index.Action, element, limits[index.Action])
+// 		}
+// 	}
+// 	// Prints the report line on the screen / in the log file
+// 	if reportTag != "" {
+// 		log.Println(strings.TrimSuffix(reportTag, " - "))
+// 	}
+// }
+
+// // Builds the report prints it and sends it
+// func buildReport() {
+// 	reportAsString := ""
+// 	for index, element := range report {
+// 		var times string
+// 		if element == 1 {
+// 			times = "time"
+// 		} else {
+// 			times = "times"
+// 		}
+// 		if index.Action == "like" {
+// 			reportAsString += fmt.Sprintf("#%s has been liked %d %s\n", index.Tag, element, times)
+// 		} else {
+// 			reportAsString += fmt.Sprintf("#%s has been %sed %d %s\n", index.Tag, index.Action, element, times)
+// 		}
+// 	}
+
+// 	// Displays the report on the screen / log file
+// 	fmt.Println(reportAsString)
+
+// 	// Sends the report to the email in the config file, if the option is enabled
+// 	send(reportAsString, true)
+// }
+
+// Likes an image, if not liked already
+func likeImage(insta *goinsta.Instagram, image response.MediaItemResponse) bool {
+	log.Println("Liking the picture")
+	if !image.HasLiked {
+		insta.Like(image.ID)
+		log.Println("Liked")
+		return true
+	} else {
+		log.Println("Image already liked")
+		return false
 	}
 }
 
-// Builds the report prints it and sends it
-func buildReport() {
-	reportAsString := ""
-	for index, element := range report {
-		var times string
-		if element == 1 {
-			times = "time"
-		} else {
-			times = "times"
-		}
-		if index.Action == "like" {
-			reportAsString += fmt.Sprintf("#%s has been liked %d %s\n", index.Tag, element, times)
-		} else {
-			reportAsString += fmt.Sprintf("#%s has been %sed %d %s\n", index.Tag, index.Action, element, times)
-		}
+// Comments an image
+func commentImage(insta *goinsta.Instagram, image response.MediaItemResponse, comments []string) {
+	rand.Seed(time.Now().Unix())
+	text := comments[rand.Intn(len(comments))]
+	insta.Comment(image.ID, text)
+	log.Println("Commented " + text)
+}
+
+// Follows a user, if not following already
+func followUser(insta *goinsta.Instagram, userInfo response.GetUsernameResponse) bool {
+	user := userInfo.User
+	log.Printf("Following %s\n", user.Username)
+	userFriendShip, err := insta.UserFriendShip(user.ID)
+	if err != nil {
+		log.Println("Could not verify friendship", user.ID)
+		return false
+	}
+	// If not following already
+	if !userFriendShip.Following {
+		insta.Follow(user.ID)
+		log.Println("Followed")
+		return true
+	} else {
+		log.Println("Already following " + user.Username)
+		return false
+	}
+}
+
+func syncFollowers(insta *goinsta.Instagram) {
+	following, err := insta.SelfTotalUserFollowing()
+	if err != nil {
+		log.Println("Could not get user following", err)
+		return
+	}
+	followers, err := insta.SelfTotalUserFollowers()
+	if err != nil {
+		log.Println("Could not get user followers", err)
+		return
 	}
 
-	// Displays the report on the screen / log file
-	fmt.Println(reportAsString)
+	var users []response.User
+	for _, user := range following.Users {
+		if !contains(followers.Users, user) {
+			users = append(users, user)
+		}
+	}
+	fmt.Printf("\n%d users are not following you back!\n", len(users))
+	// answer := getInput("Do you want to unfollow these users? [yN]")
+	// if answer != "y" {
+	// 	fmt.Println("Not unfollowing.")
+	// 	os.Exit(0)
+	// }
+	for _, user := range users {
+		fmt.Printf("Unfollowing %s\n", user.Username)
+		insta.UnFollow(user.ID)
+		time.Sleep(6 * time.Second)
+	}
+}
 
-	// Sends the report to the email in the config file, if the option is enabled
-	send(reportAsString, true)
+func contains(slice []response.User, user response.User) bool {
+	for _, currentUser := range slice {
+		if currentUser == user {
+			return true
+		}
+	}
+	return false
 }
